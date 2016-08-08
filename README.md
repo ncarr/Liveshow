5347fb6af9d1c51482ab440a5379b1151a664516
Nicholas Carr <nicholas.carr@rogers.com>
2016-08-07 22:36:29 -0400

    Now properly loads database content
    The list of slides now lists actual slides, the editor view now shows
    actual slides and actual screens, slides and screens are actually saved.
    The UI is now at least partially functional

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ A real-time collaborative presenter view and presentation editor. Currently in d
 ## Running the server
 
 `node server.js`
+
+You currently need to provision the ShareJS instance by setting the 'home' document to the contents of emptypresentation.json because I haven't built a way to automate that yet. You can do this from the client's console once it loads and errors out.
+
+```JS
+sharejs.open('home', 'json', function(error, doc) {
+  doc.set({"title":"Untitled document","id":"0","presentations":{},"background":"#00bcd4","foreground":"#000000","accent":"#ff4081","accent-foreground":"#ffffff","content":{"slides":[[{"style":"presentation-title","content":[{"style":"title","content":"Add a title"}]},{"style":"speaker","content":[{"style":"title","content":"John Doe"},{"style":"subheading","content":"@example"},{"style":"subheading","content":"Job Title"},{"style":"supporting-text","content":"Add a short bio"}]},{"style":"content","content":[{"style":"title","content":"Add a title"},{"style":"supporting-text","content":"Add some content"}]}]]}});
+```
--- public/script.js
+++ public/script.js
@@ -13,29 +13,71 @@ window.onload = function() {
             title.blur();
             pushTitle();
           }
-        };
+        }
 
         function pullTitle() {
           title.value = name.get();
-        };
+        }
 
         function pushTitle() {
           name.set(title.value);
-        };
+        }
 
         pullTitle();
+
+        var content = doc.at('content').get();
+        var slides = content.slides;
+
+        function renderSlideList(slides) {
+          $.each(slides[0], function(index, value) {
+            $('<a class="mdl-navigation__link" href="#">Slide ' + (index + 1).toString() + '</a>').appendTo($(".mdl-navigation")).click(function(e) {
+              renderSlide(index);
+            });
+          });
+        }
+
+        renderSlideList(slides);
+
+        function renderSlide(index) {
+          var slidewrapper = $(".main-slides > .slide-wrapper:first").clone();
+          $(".main-slides").empty();
+          $.each(slides, function(key, value) {
+            $(".pagenum", slidewrapper).html("Display " + (key + 1).toString());
+            slide = value[index];
+            $(".slide", slidewrapper).addClass(slide["style"]).empty();
+            $.each(slide["content"], function(index, val) {
+              if (val["style"] == "title") {
+                slidewrapper.find(".slide").append('<div class="mdl-card__title mdl-card--expand"><h2 class="mdl-card__title-text" contenteditable>' + val["content"] + '</h2></div>')
+              } else if (val["style"] == "subheading") {
+                slidewrapper.find(".mdl-card__title").addClass("subheading").append('<h6 class="flush" contenteditable>' + val["content"] + '</h6>')
+              } else {
+                slidewrapper.find(".slide").append('<div class="mdl-card__supporting-text" contenteditable>' + val["content"] + '</div>')
+              }
+            });
+            slidewrapper.appendTo($(".main-slides"));
+          });
+        }
+
+        renderSlide(0);
 
         $(".new-slide").click(function() {
           $('.mdl-navigation').each(function(){
             var new_data = $('a:last', this).clone();
-            new_data.html("Slide " + ($(this).children().length + 1).toString())
+            new_data.html("Slide " + ($(this).children().length + 1).toString());
             new_data.appendTo(this);
+            screens = doc.at(['content', 'slides']).get();
+            $.each(screens, function(index, value) {
+              doc.at(['content', 'slides', index]).push({"style": "content", "content": [{"style": "title", "content": "Add a title"}, {"style": "supporting-text", "content": "Add some content"}]});
+            });
           });
         });
         $(".new-screen").click(function() {
           var new_data = $('.main-slides > .slide-wrapper:last').clone();
-          $(new_data).children("span.pagenum").html("Display " + ($(".main-slides").children().length + 1).toString())
+          $(new_data).children("span.pagenum").html("Display " + ($(".main-slides").children().length + 1).toString());
           new_data.appendTo($(".main-slides"));
+          screens = doc.at(['content', 'slides']);
+          snapshot = screens.get();
+          screens.push(snapshot[snapshot.length - 1]);
         });
     });
 };
