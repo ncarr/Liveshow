adb872aa37c5f143c1d10d07a2c30150644e0ed4
Nicholas Carr <nicholas.carr@rogers.com>
2016-08-27 20:32:21 -0400

    Stabilized ShareDB client code

--- client.js
+++ client.js
@@ -13,126 +13,7 @@ var connection = new sharedb.Connection(socket);
 var doc = connection.get('liveshow', 'home');
 
 // Get initial value of document and subscribe to changes
-doc.subscribe(function () {
-  function renderSlide(index) {
-    var slidewrapper = $(".main-slides > .slide-wrapper:first").clone();
-    $(".main-slides").empty();
-    for (var i = 0; i < doc.data.content.slides.length; i++) {
-      slidewrapper = slidewrapper.clone();
-      $(".pagenum", slidewrapper).html("Display " + (i + 1).toString());
-      slide = doc.data.content.slides[i][index];
-      $(".stretch", slidewrapper).empty();
-      $('<div class="mdl-card mdl-shadow--2dp slide" data-slide="' + index + '" data-display="' + i + '"></div>').addClass(slide["style"]).appendTo($(".stretch", slidewrapper))
-      for (var j = 0; j < slide.content.length; j++) {
-        if (slide.content[j]["style"] == "title") {
-          slidewrapper.find(".slide").append('<div class="mdl-card__title mdl-card--expand"><textarea rows="1" class="mdl-card__title-text" placeholder="Add a title">' + escapeHtml(slide.content[j]["content"]) + '</textarea></div>')
-        } else if (slide.content[j]["style"] == "subheading") {
-          slidewrapper.find(".mdl-card__title").addClass("subheading").append('<textarea rows="1" class="subheading">' + escapeHtml(slide.content[j]["content"]) + '</textarea>')
-        } else {
-          slidewrapper.find(".slide").append('<textarea class="mdl-card__supporting-text">' + escapeHtml(slide.content[j]["content"]) + '</textarea>')
-        }
-      }
-      slidewrapper.appendTo($(".main-slides"));
-    }
-
-    $(".main-slides .mdl-card__title-text").blur(function() {
-      uploadSlideTitle($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), $(this).val());
-    });
-
-    $(".main-slides .mdl-card__supporting-text").blur(function() {
-      uploadSlideContent($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), ($(this).parents(".slide").hasClass("speaker") ? 3 : 1), $(this).val());
-    });
-    $(".main-slides .subheading").blur(function() {
-      uploadSlideContent($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), $(this).index(), $(this).val());
-    });
-  }
-  global.renderSlide = renderSlide
-
-  function uploadSlideTitle(display, slide, content) {
-    doc.submitOp([{p: ["content", "slides", display, slide, "content", 0, "content"], od: doc.data.content.slides[display][slide].content[0].content,  oi: content}]);
-  }
-  global.uploadSlideTitle = uploadSlideTitle;
-  function uploadSlideContent(display, slide, pos, content) {
-    doc.submitOp([{p: ["content", "slides", display, slide, "content", pos, "content"], od: doc.data.content.slides[display][slide].content[pos].content,  oi: content}]);
-  }
-  global.uploadSlideContent = uploadSlideContent;
-
-  function pushTitle() {
-    doc.submitOp([{p: ["title"], od: doc.data.title,  oi: $("#title").val()}]);
-  }
-  $("#title").blur(pushTitle);
-  $("#title").keypress(function(event) {
-    if (event.keyCode === 13) {
-      title.blur();
-      pushTitle();
-    }
-  });
-
-  $(".new-slide").click(function() {
-    showNewSlide();
-    screens = doc.data.content.slides;
-    for (var i = 0; i < screens.length; i++) {
-      doc.submitOp([{p: ['content', 'slides', i, screens.length], li: {"style": "content", "content": [{"style": "title", "content": "Add a title"}, {"style": "supporting-text", "content": "Add some content"}]}}]);
-    }
-  });
-  $(".new-screen").click(function() {
-    showNewDisplay();
-    screens = doc.data.content.slides;
-    doc.submitOp([{p: ["content", "slides", screens.length], li: screens[screens.length - 1]}]);
-  });
-
-  $(".primary-colour-list > li").click(function() {
-    var colour = $("button", this).css("background-color");
-    updateSlideColour("primary", colour);
-    doc.submitOp([{p: ["background"], od: doc.data.background, oi: colour}]);
-  });
-  $(".accent-colour-list > li").click(function() {
-    var colour = $("button", this).css("background-color");
-    updateSlideColour("accent", colour);
-    doc.submitOp([{p: ["accent"], od: doc.data.accent, oi: colour}]);
-  });
-  $(".text-colour-list > li").click(function() {
-    var colour = $("button", this).css("background-color");
-    updateSlideColour("text", colour);
-    doc.submitOp([{p: ["foreground"], od: doc.data.foreground, oi: colour}]);
-  });
-
-  $("#global-colours").change(function(e) {
-    screens = doc.at(['content', 'slides']).get();
-    slide = $(this).parents(".slide").data("slide");
-    if (this.checked) {
-      for (var i = 0; i < screens.length; i++) {
-        doc.submitOp([{p: ["content", "slides", i, slide, "primary"], oi: doc.data.primary}]);
-        doc.submitOp([{p: ["content", "slides", i, slide, "accent"], oi: doc.data.accent}]);
-        doc.submitOp([{p: ["content", "slides", i, slide, "foreground"], oi: doc.data.foreground}]);
-      }
-    } else {
-      for (var i = 0; i < screens.length; i++) {
-        doc.submitOp([{p: ["content", "slides", i, slide, "primary"], od: doc.data.content.slides[i][slide].primary}]);
-        doc.submitOp([{p: ["content", "slides", i, slide, "accent"], od: doc.data.content.slides[i][slide].accent}]);
-        doc.submitOp([{p: ["content", "slides", i, slide, "foreground"], od: doc.data.content.slides[i][slide].foreground}]);
-      }
-    }
-  });
-
-  function showNewSlide() {
-    $('.mdl-navigation').each(function(){
-      var new_data = $('a:last', this).clone();
-      new_data.html("Slide " + ($(this).children().length + 1).toString());
-      new_data.appendTo(this).click(function(e) {
-        renderSlide($(this).children().length);
-      });
-    });
-  }
-
-  function renderSlideList(slides) {
-    for (var i = 0; i < slides[0].length; i++) {
-      $('<a class="mdl-navigation__link" href="#">Slide ' + (i + 1).toString() + '</a>').appendTo($(".mdl-navigation")).click(function(e) {
-        renderSlide(i);
-      });
-    }
-  }
-});
+doc.subscribe();
 // When document changes (by this client or any other, or the server)
 doc.on('op', callback);
 
@@ -158,17 +39,26 @@ function callback(op) {
       if (op[i].p.length == 3 && op[i].p[1] == "slides" && op[i].li) {
         showNewDisplay();
       }
-      if (op[i].p.length == 7 && op[i].p[1] == "slides" && op[i].p[5] == 0) {
-        $(".main-slides .slide:nth-child(" + (op[i].p[2] + 1) + ") .mdl-card__title-text").val(op[i].oi);
+      if (op[i].p.length == 6 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && op[i].p[5] == "background") {
+        updateSlideColour("primary", op[i].oi);
       }
-      if (op[i].p.length == 7 && op[i].p[1] == "slides" && (op[i].p[5] == 1 || op[i].p[5] == 3)) {
-        if (doc.at(op[i].p.slice(0, 6).concat("style")).get() == "subheading") {
+      if (op[i].p.length == 6 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && op[i].p[5] == "accent") {
+        updateSlideColour("accent", op[i].oi);
+      }
+      if (op[i].p.length == 6 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && op[i].p[5] == "foreground") {
+        updateSlideColour("text", op[i].oi);
+      }
+      if (op[i].p.length == 7 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && op[i].p[5] == 0) {
+        $(".main-slides .slide:nth-child(" + (op[i].p[2] + 1) + ") .mdl-card__title-text").not(".subheading").val(op[i].oi);
+      }
+      if (op[i].p.length == 7 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && (op[i].p[5] == 1 || op[i].p[5] == 3)) {
+        if (doc.data.content.slides[op[i].p[2]][op[i].p[3]].content[op[i].p[5]].style == "subheading") {
           $(".main-slides .slide:nth-child(" + (op[i].p[2] + 1) + ") textarea.subheading:nth-child(" + (op[i].p[5] + 1) + ")").val(op[i].oi);
         } else {
           $(".main-slides .slide:nth-child(" + (op[i].p[2] + 1) + ") .mdl-card__supporting-text").val(op[i].oi);
         }
       }
-      if (op[i].p.length == 7 && op[i].p[1] == "slides" && op[i].p[5] == 2) {
+      if (op[i].p.length == 7 && op[i].p[1] == "slides" && op[i].p[3] == $(".main-slides .slide:first").data("slide") && op[i].p[5] == 2) {
         $(".main-slides .slide:nth-child(" + (op[i].p[2] + 1) + ") textarea.subheading:nth-child(" + (op[i].p[5] + 1) + ")").val(op[i].oi);
       }
     }
@@ -186,6 +76,118 @@ function escapeHtml(text) {
   return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
+function renderSlide(index) {
+  var slidewrapper = $(".main-slides > .slide-wrapper:first").clone();
+  $(".main-slides").empty();
+  doc.fetch(function (err) {
+    if (err) throw err;
+    for (var i = 0; i < doc.data.content.slides.length; i++) {
+      slidewrapper = slidewrapper.clone();
+      $(".pagenum", slidewrapper).html("Display " + (i + 1).toString());
+      slide = doc.data.content.slides[i][index];
+      $(".stretch", slidewrapper).empty();
+      $('<div class="mdl-card mdl-shadow--2dp slide" data-slide="' + index + '" data-display="' + i + '"></div>').addClass(slide.style).appendTo($(".stretch", slidewrapper));
+      for (var j = 0; j < slide.content.length; j++) {
+        if (slide.content[j]["style"] == "title") {
+          slidewrapper.find(".slide").append('<div class="mdl-card__title mdl-card--expand"><textarea rows="1" class="mdl-card__title-text" placeholder="Add a title">' + escapeHtml(slide.content[j]["content"]) + '</textarea></div>');
+        } else if (slide.content[j]["style"] == "subheading") {
+          slidewrapper.find(".mdl-card__title").addClass("subheading").append('<textarea rows="1" class="subheading mdl-card__title-text">' + escapeHtml(slide.content[j]["content"]) + '</textarea>');
+        } else {
+          slidewrapper.find(".slide").append('<textarea class="mdl-card__supporting-text">' + escapeHtml(slide.content[j]["content"]) + '</textarea>');
+        }
+      }
+      slidewrapper.appendTo($(".main-slides"));
+    }
+    updateSlideColour("primary", doc.data.content.slides[0][index].background || doc.data.background);
+    updateSlideColour("accent", doc.data.content.slides[0][index].accent || doc.data.accent);
+    updateSlideColour("text", doc.data.content.slides[0][index].foreground || doc.data.foreground);
+    $(".main-slides .mdl-card__title-text").not(".subheading").blur(function() {
+      console.log("blur");
+      uploadSlideTitle($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), $(this).val());
+    });
+    $(".main-slides .mdl-card__supporting-text").blur(function() {
+      uploadSlideContent($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), ($(this).parents(".slide").hasClass("speaker") ? 3 : 1), $(this).val());
+    });
+    $(".main-slides .subheading").blur(function() {
+      uploadSlideContent($(this).parents(".slide").data("display"), $(this).parents(".slide").data("slide"), $(this).index(), $(this).val());
+    });
+  });
+}
+global.renderSlide = renderSlide
+
+function renderSlideList(slides) {
+  for (var i = 0; i < slides[0].length; i++) {
+    $('<a class="mdl-navigation__link" href="#">Slide ' + (i + 1).toString() + '</a>').appendTo($(".mdl-navigation")).click(function(e) {
+      renderSlide(i);
+    });
+  }
+}
+
+function uploadSlideTitle(display, slide, content) {
+  console.log("blur");
+  doc.fetch(function (err) {
+    if (err) throw err;
+    doc.submitOp([{p: ["content", "slides", display, slide, "content", 0, "content"], od: doc.data.content.slides[display][slide].content[0].content,  oi: content}]);
+  });
+}
+global.uploadSlideTitle = uploadSlideTitle;
+function uploadSlideContent(display, slide, pos, content) {
+  console.log("blur");
+  doc.fetch(function (err) {
+    if (err) throw err;
+    doc.submitOp([{p: ["content", "slides", display, slide, "content", pos, "content"], od: doc.data.content.slides[display][slide].content[pos].content,  oi: content}]);
+  });
+}
+global.uploadSlideContent = uploadSlideContent;
+
+function pushTitle() {
+  doc.fetch(function (err) {
+    if (err) throw err;
+    doc.submitOp([{p: ["title"], od: doc.data.title,  oi: $("#title").val()}]);
+  });
+}
+$("#title").blur(pushTitle);
+$("#title").keypress(function(event) {
+  if (event.keyCode === 13) {
+    title.blur();
+    pushTitle();
+  }
+});
+
+function showNewSlide() {
+  $('.mdl-navigation').each(function(){
+    var new_data = $('a:last', this).clone();
+    new_data.html("Slide " + ($(this).children().length + 1).toString());
+    new_data.appendTo(this).click(function(e) {
+      renderSlide($(this).children().length);
+    });
+  });
+}
+$(".new-slide").click(function() {
+  showNewSlide();
+  doc.fetch(function (err) {
+    if (err) throw err;
+    screens = doc.data.content.slides;
+    for (var i = 0; i < screens.length; i++) {
+      doc.submitOp([{p: ['content', 'slides', i, screens.length], li: {"style": "content", "content": [{"style": "title", "content": "Add a title"}, {"style": "supporting-text", "content": "Add some content"}]}}]);
+    }
+  });
+});
+
+function showNewDisplay() {
+  var new_data = $('.main-slides > .slide-wrapper:last').clone();
+  $(new_data).children("span.pagenum").html("Display " + ($(".main-slides").children().length + 1).toString());
+  new_data.appendTo($(".main-slides"));
+}
+$(".new-screen").click(function() {
+  showNewDisplay();
+  doc.fetch(function (err) {
+    if (err) throw err;
+    screens = doc.data.content.slides;
+    doc.submitOp([{p: ["content", "slides", screens.length], li: screens[screens.length - 1]}]);
+  });
+});
+
 function updateSlideColour(type, colour) {
   if (type == "primary") {
     $("#primary-colour-menu").css("background-color", colour);
@@ -201,10 +203,43 @@ function updateSlideColour(type, colour) {
   }
 }
 
-function showNewDisplay() {
-  var new_data = $('.main-slides > .slide-wrapper:last').clone();
-  $(new_data).children("span.pagenum").html("Display " + ($(".main-slides").children().length + 1).toString());
-  new_data.appendTo($(".main-slides"));
-}
+$(".primary-colour-list > li").click(function() {
+  var colour = $("button", this).css("background-color");
+  updateSlideColour("primary", colour);
+  doc.fetch(function (err) {
+    if (err) throw err;
+    doc.submitOp([{p: ["background"], od: doc.data.background, oi: colour}]);
+  });
+});
+$(".accent-colour-list > li").click(function() {
+  var colour = $("button", this).css("background-color");
+  updateSlideColour("accent", colour);
+  doc.submitOp([{p: ["accent"], od: doc.data.accent, oi: colour}]);
+});
+$(".text-colour-list > li").click(function() {
+  var colour = $("button", this).css("background-color");
+  updateSlideColour("text", colour);
+  doc.submitOp([{p: ["foreground"], od: doc.data.foreground, oi: colour}]);
+});
+
+$("#global-colours").change(function(e) {
+  slide = $(this).parents(".slide").data("slide");
+  doc.fetch(function (err) {
+    if (err) throw err;
+    if (this.checked) {
+      for (var i = 0; i < doc.data.content.slides.length; i++) {
+        doc.submitOp([{p: ["content", "slides", i, slide, "primary"], oi: doc.data.primary}]);
+        doc.submitOp([{p: ["content", "slides", i, slide, "accent"], oi: doc.data.accent}]);
+        doc.submitOp([{p: ["content", "slides", i, slide, "foreground"], oi: doc.data.foreground}]);
+      }
+    } else {
+      for (var i = 0; i < doc.data.content.slides.length; i++) {
+        doc.submitOp([{p: ["content", "slides", i, slide, "primary"], od: doc.data.content.slides[i][slide].primary}]);
+        doc.submitOp([{p: ["content", "slides", i, slide, "accent"], od: doc.data.content.slides[i][slide].accent}]);
+        doc.submitOp([{p: ["content", "slides", i, slide, "foreground"], od: doc.data.content.slides[i][slide].foreground}]);
+      }
+    }
+  });
+});
 
 $("#title").autoGrowInput();
